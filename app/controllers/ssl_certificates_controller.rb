3a348399ac8addcfef8123453d671a3d6740429b
ajwann <ajwann@users.noreply.github.com>
2017-07-16 14:38:38 -0400

    use letsencrypt for ssl

---
+++ app/controllers/ssl_certificates_controller.rb
@@ -0,0 +1,7 @@
+class SslCertificatesController < ApplicationController
+
+  def letsencrypt
+    render plain: 'xrXJjczoBkcC6IdjRfHaauucSXBFj9zji_JkTe5PRN4.gRfRobaJJWWVuW3_tyomWzvJvLvutWEqVHfqexKeFlo'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
+  get '/.well-known/acme-challenge/xrXJjczoBkcC6IdjRfHaauucSXBFj9zji_JkTe5PRN4' => 'ssl_certificates#letsencrypt'
   get 'admin/home'
   get 'landing/index'
 
